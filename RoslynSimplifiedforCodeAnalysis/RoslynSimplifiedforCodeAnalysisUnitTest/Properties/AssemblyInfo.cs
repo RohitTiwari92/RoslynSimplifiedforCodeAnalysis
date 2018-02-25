922cd04856edb244e9a4a9a727644a5700a591d8
Rohit Tiwari <rohit.tiwari@logisure.in>
2018-02-25 12:43:47 +0530

    project setup

---
+++ RoslynSimplifiedforCodeAnalysis/RoslynSimplifiedforCodeAnalysisUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoslynSimplifiedforCodeAnalysisUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoslynSimplifiedforCodeAnalysisUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c0a0046-b42f-44e4-9a28-503888315510")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
