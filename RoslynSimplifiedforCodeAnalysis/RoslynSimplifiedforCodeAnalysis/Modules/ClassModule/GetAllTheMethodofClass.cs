736525edfa8a4f59eb6b0b50b30c76161a918c1e
Rohit Tiwari <rohit.beawar@gmail.com>
2018-03-12 18:19:48 +0530

    Method syntex

---
+++ RoslynSimplifiedforCodeAnalysis/RoslynSimplifiedforCodeAnalysis/Modules/ClassModule/GetAllTheMethodofClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynSimplifiedforCodeAnalysis.Modules.ClassModule
+{
+  public  class GetAllTheMethodofClass
+    {
+        public List<string> GetClassNameList(ClassDeclarationSyntax classsyn)
+        {
+            IEnumerable<MethodDeclarationSyntax> methods = GetMethodModelList(classsyn);
+            List<string> MethodNames = new List<string>();
+            foreach (var method in methods)
+            {
+                MethodNames.Add(method.Identifier.ValueText);
+            }
+            return MethodNames;
+        }
+
+        public List<MethodDeclarationSyntax> GetMethodModelList(ClassDeclarationSyntax classsyn)
+        {
+            
+            return classsyn.SyntaxTree.GetRoot().DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
+        }
+    }
+}
