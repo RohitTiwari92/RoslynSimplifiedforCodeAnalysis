0f2a92f38841020e8d78fd6f89708f97b840a863
Rohit Tiwari <rohit.beawar@gmail.com>
2018-03-16 16:06:36 +0530

    init helper function added

---
+++ RoslynSimplifiedforCodeAnalysis/RoslynSimplifiedforCodeAnalysis/Modules/NamespaceModule/GettheNamespaceListFromProjectModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RoslynSimplifiedforCodeAnalysis.Modules.ClassModule;
+
+namespace RoslynSimplifiedforCodeAnalysis.Modules.NamespaceModule
+{
+   public class GettheNamespaceListFromProjectModel
+    {
+        public List<string> GetNamespaceNameList(Compilation Comproj)
+        {
+            List<string> NamespaceNames = new List<string>();
+            foreach (var syntaxTree in Comproj.SyntaxTrees)
+            {
+                var NamespaceVisitor = new NamespaceVirtualizationVisitor();
+                NamespaceVisitor.Visit(syntaxTree.GetRoot());
+                foreach (var item in NamespaceVisitor.Namespaces)
+                {
+                    NamespaceNames.Add( GetFullName(item));
+                }
+                
+            }
+            return NamespaceNames;
+        }
+        public static string GetFullName(NamespaceDeclarationSyntax node)
+        {
+           
+                return String.Format("{0}.{1}",
+                    GetFullName((NamespaceDeclarationSyntax)node.Parent),
+                    ((IdentifierNameSyntax)node.Name).Identifier.ToString());
+            
+        }
+        public List<NamespaceDeclarationSyntax> GetNamespaceModelList(Compilation Comproj)
+        {
+            List<NamespaceDeclarationSyntax> NamespaceModel = new List<NamespaceDeclarationSyntax>();
+            foreach (var syntaxTree in Comproj.SyntaxTrees)
+            {
+                var NamespaceVisitor = new NamespaceVirtualizationVisitor();
+                NamespaceVisitor.Visit(syntaxTree.GetRoot());
+                NamespaceModel.AddRange(NamespaceVisitor.Namespaces);
+            }
+            return NamespaceModel;
+        }
+    }
+}
---
+++ RoslynSimplifiedforCodeAnalysis/RoslynSimplifiedforCodeAnalysis/Modules/NamespaceModule/NamespaceVirtualizationVisitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RoslynSimplifiedforCodeAnalysis.Modules.NamespaceModule
+{
+    public class NamespaceVirtualizationVisitor : CSharpSyntaxRewriter
+    {
+        public NamespaceVirtualizationVisitor()
+        {
+            Namespaces = new List<NamespaceDeclarationSyntax>();
+        }
+
+        public List<NamespaceDeclarationSyntax> Namespaces { get; set; }
+
+        public  SyntaxNode VisitClassDeclaration(NamespaceDeclarationSyntax node)
+        {
+            node = (NamespaceDeclarationSyntax)VisitNamespaceDeclaration(node);
+            Namespaces.Add(node); // save your visited classes
+            return node;
+        }
+    }
+}
