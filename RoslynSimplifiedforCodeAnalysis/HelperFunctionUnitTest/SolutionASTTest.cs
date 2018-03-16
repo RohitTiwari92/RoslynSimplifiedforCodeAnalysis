45324d41ff5f025fdbdcfa2b36adec4972f18145
Rohit Tiwari <rohit.beawar@gmail.com>
2018-03-16 17:48:45 +0530

    init helper test  function added

---
+++ RoslynSimplifiedforCodeAnalysis/HelperFunctionUnitTest/SolutionASTTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoslynSimplifiedforCodeAnalysis.Modules.SolutionModule;
+using System.IO;
+namespace HelperFunctionUnitTest
+{
+    [TestClass]
+    public class SolutionASTTest
+    {
+        private static string solutionpath { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext con)
+
+        {
+            solutionpath = ConfigurationManager.AppSettings["TestPath"]+ "RoslynSimplifiedforCodeAnalysis.sln";
+        }
+
+        [TestMethod]
+        public void TestMethodSolutionAST()
+        {
+            AST sast=new AST();
+            var res = sast.GetAsTfromSolutionFile(solutionpath);
+            Assert.IsNotNull(res);
+        }
+    }
+}
